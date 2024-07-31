ba02f57ec68963753937b1a1c5e290dcfd3d7eb0
Keila <keilaprado@alunos.utfpr.edu.br>
2024-07-31 13:48:08 -0300

    refactor: restructing myMovies navigator

--- frontend/src/components/MovieInformation/MovieInformation.tsx
+++ frontend/src/components/MovieInformation/MovieInformation.tsx
@@ -1,28 +1,27 @@
 import styles from './MovieInformation.module.css'
-import heart from '../../assets/heart.png'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const MovieInformation = () => {
   return (
     <div className={styles.alignContainer}>
         <div className={styles.main}>
-        <img className={styles.photo} src={heart} />
-        <div className={styles.text}>
-            <span>Interstellar</span>
-            <span>Christopher Nolan</span>
-            <span>198min</span>
-            <span>Sci-fi</span>
-            <span>2014</span>
-            <span>08-dec-2021</span>
-        </div>
-        <div className={styles.actions}>
+          <div className={styles.photo}>MM</div>
+          <div className={styles.text}>
+              <span>Interstellar</span>
+              <span>Christopher Nolan</span>
+              <span>198min</span>
+              <span>Sci-fi</span>
+              <span>2014</span>
+              <span>08-dec-2021</span>
+          </div>
+          <div className={styles.actions}>
             <button className={styles.button}>
-            <i className="fas fa-edit"></i>
+              <i className="fas fa-edit"></i>
             </button>
             <button className={styles.button}>
-            <i className="fas fa-trash"></i>
+              <i className="fas fa-trash"></i>
             </button>
-        </div>
+          </div>
         </div>
     </div>
     
--- frontend/src/pages/MyMovies/MyMovies.tsx
+++ frontend/src/pages/MyMovies/MyMovies.tsx
@@ -1,18 +1,38 @@
-import MovieInformation from "../../components/MovieInformation/MovieInformation"
+import MovieInformation from "../../components/MovieInformation/MovieInformation";
+import SearchBar from "../../components/SearchBar/SearchBar"
+import { IoIosSearch } from "react-icons/io";
 import Sidebar from "../../components/SideBar/Sidebar"
 import styles from "./MyMovies.module.css"
+import Modal from "../../components/Modal/Modal";
+import { useState } from "react";
 
 const MyMovies = () => {
+
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  
   return (
-    <>
+    <div className={styles.movieContent}>
         <Sidebar />
-        <div className={styles.container}>
-            <MovieInformation />
-            <MovieInformation />
-            <MovieInformation />
-            <MovieInformation />
+        <div className={styles.mainContent}>
+          <SearchBar Icon={IoIosSearch} />
+          <div className={styles.tb}>
+            <h2>My Movies</h2>
+            <button className={styles.buttonAdd} onClick={() => setOpenModal(true)}>ADD NEW MOVIE</button>
+            <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}/>
+          </div>
+          <div className={styles.container}>
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+              <MovieInformation />
+          </div>
         </div>
-    </>
+    </div>
     
   )
 }
