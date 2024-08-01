c32ec4454a2a101814065dbf74989cf5eee077c7
Jonas Tomaz <jonas.aquinos@hotmail.com>
2024-08-01 17:29:10 -0300

    feat: adding popcorn gif to loading components

--- frontend/src/pages/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/pages/ProtectedRoute/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
+import popcorn from "../../assets/popcorn.gif";
+import styles from "./ProtectedRoute.module.css";
 
 type ProtectedRouteProps = {
     element: React.ReactElement;
@@ -11,7 +13,11 @@ export function ProtectedRoute({ element }: ProtectedRouteProps) {
     const location = useLocation();
 
     if (loading) {
-        return <div>Loading...</div>;
+        return (
+            <div className={styles.container}>
+                <img src={popcorn} alt="Loading..." />
+            </div>
+        );
     }
 
     if (isAuthenticated) {
